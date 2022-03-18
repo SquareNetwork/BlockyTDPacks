70607539a8800d9dbf592b95a08e9fa7dbace95a
SquaredStudios-MC <67833955+SquaredStudios-MC@users.noreply.github.com>
2022-03-17 23:05:59 -0400

    change download URL

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const fs = require('fs');
 require('dotenv').config();
 
 app.get("/", function(req, res) {
-    res.send("<h1>BlockyTD Pack Downloader by AstroSquared</h1>Download: /download/:version")
+    res.send("<h1>BlockyTD Pack Downloader by AstroSquared</h1>Download: /download/:version.zip")
 });
 
 app.get("/download/:version", function(req, res) {
-    res.sendFile(__dirname + "/packs/BlockyTD." + req.params.version + ".zip");
+    res.sendFile(__dirname + "/packs/BlockyTD." + req.params.version);
 });
 
 app.listen(process.env.PORT);
