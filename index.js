2061b2fc7cb34204173a755d74ae5910b79f02d9
SquaredStudios-MC <67833955+SquaredStudios-MC@users.noreply.github.com>
2022-03-17 21:21:57 -0400

    Remove download latest

--- index.js
+++ index.js
@@ -3,12 +3,7 @@ const fs = require('fs');
 require('dotenv').config();
 
 app.get("/", function(req, res) {
-    res.send("<h1>BlockyTD Pack Downloader by AstroSquared</h1>Latest: /download <br>Version: /download/:version")
-});
-
-app.get("/download", function(req, res) {
-    var latest = fs.readFileSync(__dirname + "/packs/latest.txt").toString();
-    res.sendFile(__dirname + "/packs/BlockyTD." + latest + ".zip");
+    res.send("<h1>BlockyTD Pack Downloader by AstroSquared</h1>Download: /download/:version")
 });
 
 app.get("/download/:version", function(req, res) {
